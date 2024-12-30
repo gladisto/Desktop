8f865722259dbe713c433e4f56680290b68586eb
gladisto <gladistone.ribeiro.lima@hotmail.com>
2024-12-30 11:01:26 -0300

    gladiston

---
+++ To do List/scripts.js
@@ -0,0 +1,16 @@
+function adicionarTarefe(){
+    let valorDoInput = document.querySelector("input").value
+
+
+    let li = document.createElement("li")
+    li.innerHTML = valorDoInput + '<span onclick="deletarTarefa(this)">❌</span>'
+
+  document.querySelector("ul").appendChild(li)
+
+
+  document.querySelector("input").value = ''
+}
+    
+function deletarTarefa(li){
+  li.parentElement.remove()
+}
